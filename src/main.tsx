2151370ab2d1cf4afbb9b7041e03c961b32ea002
Erik Vagil <erikvagil2734@gmail.com>
2025-01-29 19:35:12 -0700

    Created landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css";
+import emailjs from "@emailjs/browser";
 
-createRoot(document.getElementById('root')!).render(
+emailjs.init({
+  publicKey: '7H_LGaw0hFK8OQVff',
+  blockHeadless: true,
+  limitRate: {
+    id: 'app',
+    throttle: 10000,
+  },
+});
+
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
